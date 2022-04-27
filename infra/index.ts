4608563c2ff28236ea9d5a1b789e2a913a5a949f
Dave Burke <dburke84@gmail.com>
2022-04-26 21:52:48 -0500

    Add route53 record

--- infra/index.ts
+++ infra/index.ts
@@ -1,8 +1,34 @@
-// import * as pulumi from '@pulumi/pulumi'
+import * as pulumi from '@pulumi/pulumi'
 import * as aws from '@pulumi/aws'
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket('rpn-calc')
+const stack = pulumi.getStack()
+const subDomain = stack === 'prod' ? 'rpn' : `rpn-${stack}`
+const domain = `${subDomain}.superfun.link`
 
-// Export the name of the bucket
-export const bucketName = bucket.id
+const siteBucket = new aws.s3.Bucket(domain, {
+  bucket: domain,
+  websiteDomain: domain,
+  website: {
+    indexDocument: 'index.html'
+  }
+})
+
+export const bucketName = siteBucket.id
+export const s3WebsiteUrl = siteBucket.websiteEndpoint
+
+const hostedZone = aws.route53.getZone({
+  name: 'superfun.link'
+})
+
+const record = new aws.route53.Record('rpn-calc', {
+  zoneId: hostedZone.then(hostedZone => hostedZone.zoneId),
+  name: domain,
+  type: 'A',
+  aliases: [{
+    name: siteBucket.websiteDomain,
+    zoneId: siteBucket.hostedZoneId,
+    evaluateTargetHealth: true
+  }]
+})
+
+export const recordId = record.id
