a9f94f17a7edb6f1435e5eed1ea7186725969e0c
Dave Burke <dburke84@gmail.com>
2022-05-01 12:23:26 -0500

    Add decimal.js-light
    This is decimal.js but without trig functions this project doesn't need.

--- frontend/src/services/rpn.ts
+++ frontend/src/services/rpn.ts
@@ -1,3 +1,4 @@
+import Decimal from 'decimal.js-light'
 
 function appendNumberToWip (stack: string[], x: string): string[] {
   const result = [...stack]
@@ -50,25 +51,25 @@ function swap (stack: string[]): string[] {
   return result
 }
 
-function op1 (stack: string[], op: (x: number) => number): string[] {
+function op1 (stack: string[], op: (x: Decimal) => Decimal): string[] {
   const result = stack.filter(it => it)
   if (result.length < 1) {
     return stack
   }
-  const x = Number(result.pop())
-  result.push(String(op(x)))
+  const x = new Decimal(result.pop() ?? NaN)
+  result.push(op(x).toString())
   result.push('')
   return result
 }
 
-function op2 (stack: string[], op: (a: number, b: number) => number): string[] {
+function op2 (stack: string[], op: (a: Decimal, b: Decimal) => Decimal): string[] {
   const result = stack.filter(it => it)
   if (result.length < 2) {
     return stack
   }
-  const a = Number(result.pop())
-  const b = Number(result.pop())
-  result.push(String(op(a, b)))
+  const a = new Decimal(result.pop() ?? NaN)
+  const b = new Decimal(result.pop() ?? NaN)
+  result.push(op(a, b).toString())
   result.push('')
   return result
 }
@@ -80,14 +81,14 @@ export default {
   appendNumberToWip,
   appendDecimalToWip,
   backspaceWip,
-  negate: (stack: string[]) => op1(stack, (x) => x * -1),
-  invert: (stack: string[]) => op1(stack, (x) => 1 / x),
-  timesTen: (stack: string[]) => op1(stack, (x) => x * 10),
-  overTen: (stack: string[]) => op1(stack, (x) => x / 10),
-  square: (stack: string[]) => op1(stack, (x) => Math.sqrt(x)),
-  add: (stack: string[]) => op2(stack, (a, b) => a + b),
-  subtract: (stack: string[]) => op2(stack, (a, b) => a - b),
-  multiply: (stack: string[]) => op2(stack, (a, b) => a * b),
-  divide: (stack: string[]) => op2(stack, (a, b) => b / a),
-  pow: (stack: string[]) => op2(stack, (a, b) => Math.pow(b, a))
+  negate: (stack: string[]) => op1(stack, (x) => x.times(new Decimal(-1))),
+  invert: (stack: string[]) => op1(stack, (x) => new Decimal(1).dividedBy(x)),
+  timesTen: (stack: string[]) => op1(stack, (x) => x.times(new Decimal(10))),
+  overTen: (stack: string[]) => op1(stack, (x) => x.dividedBy(new Decimal(10))),
+  square: (stack: string[]) => op1(stack, (x) => x.squareRoot()),
+  add: (stack: string[]) => op2(stack, (a, b) => a.plus(b)),
+  subtract: (stack: string[]) => op2(stack, (a, b) => a.minus(b)),
+  multiply: (stack: string[]) => op2(stack, (a, b) => a.times(b)),
+  divide: (stack: string[]) => op2(stack, (a, b) => b.dividedBy(a)),
+  pow: (stack: string[]) => op2(stack, (a, b) => b.pow(a))
 }
