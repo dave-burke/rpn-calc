3153c244b53eb23ac097d5745869ded3d81d6f40
Dave Burke <dburke84@gmail.com>
2022-04-30 21:47:03 -0500

    Configure vitest
    Setting 'globals: true' works fine on the CLI but VSCode still has a fit
    that it can't find 'test' and 'expect'. Importing manually seems to
    work, but it's an annoying workaround.
    
    VSCode also complains about the triple-slash declaration in
    vite.config.ts and says that the file isn't part of the project. I think
    it doesn't recognize the "references" entry for tsconfig.node.json that
    lists vite.config.ts. Having to ignore the config file is annoying, but
    it's a small file so the risk is not so bad I suppose.
    
    But holy crap the js/ts community needs to get its shit together.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  test: {
+    // VSCode doesn't see this and still complains :-/
+    // globals: true,
+    environment: 'jsdom'
+  }
 })
