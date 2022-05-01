71a54f5caa6946536bb432061893f8ff5cbd5663
Dave Burke <dburke84@gmail.com>
2022-05-01 12:30:19 -0500

    Replace sync-dev with full deploy script

--- infra/sync-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-bucketName=$(pulumi stack output -s dev bucketName 2>/dev/null)
-
-aws s3 sync --acl public-read --follow-symlinks --delete ../frontend/dist s3://"${bucketName}"
-
