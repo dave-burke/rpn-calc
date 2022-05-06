d4ec8e87b4358558041d7a7df7a6368a4e9e6324
Dave Burke <dburke84@gmail.com>
2022-05-05 21:05:03 -0500

    Configure icons

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,34 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA()],
+  plugins: [vue(), VitePWA({
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'RPN Calc',
+        short_name: 'An RPN Calculator',
+        description: 'A stack-based calculator that uses Reverse Polish Notation.',
+        theme_color: '#444444',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    }
+  )],
   test: {
     // VSCode doesn't see this and still complains :-/
     // globals: true,
