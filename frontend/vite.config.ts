da482bf15603a779cbc8a550e4d0d02cbb6fb19a
Dave Burke <dburke84@gmail.com>
2022-05-05 20:32:03 -0500

    Add pwa plugin to vite conf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA()],
   test: {
     // VSCode doesn't see this and still complains :-/
     // globals: true,
