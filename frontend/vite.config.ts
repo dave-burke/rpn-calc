0a5031d43e45756a5b511a4c455b0d5609392a0a
Dave Burke <dburke84@gmail.com>
2022-05-06 20:23:16 -0500

    Replace favicon

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,25 +8,25 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [vue(), VitePWA({
     registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
         name: 'RPN Calc',
-        short_name: 'An RPN Calculator',
+        short_name: 'RPN Calculator',
         description: 'A stack-based calculator that uses Reverse Polish Notation.',
         theme_color: '#444444',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
