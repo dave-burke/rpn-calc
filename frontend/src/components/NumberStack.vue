a0c352a6ad9d72f0dc1f0fd75d1a5306341eb11b
Dave Burke <dburke84@gmail.com>
2025-04-03 20:57:33 -0500

    Ensure typed input is valid

--- frontend/src/components/NumberStack.vue
+++ frontend/src/components/NumberStack.vue
@@ -2,9 +2,50 @@
 const props = defineProps<{modelValue: string[]}>()
 const emit = defineEmits(['update:modelValue'])
 
-function update (n: string, index: number) {
+const validCharsRegex = /[^0-9.-]/g
+
+function handleInput (value: string, index: number) {
+  // As the user types, don't let them type anything invalid
+  // An empty string is considered valid, for now
+
+  // Replace all invalid chars
+  value = value.replaceAll(validCharsRegex, '')
+
+  let result = ''
+  let hasDecimal = false
+
+  for (let i = 0; i < value.length; i++) {
+    const char = value[i]
+
+    // Skip negative sign if not first char
+    if (i !== 0 && char === '-') {
+      continue
+    }
+
+    // Allow only one decimal point
+    if (char === '.') {
+      if (hasDecimal) {
+        continue
+      }
+      hasDecimal = true
+    }
+
+    result += char
+  }
   const entries = [...props.modelValue]
-  entries[index] = n
+  entries[index] = result
+  emit('update:modelValue', entries)
+}
+
+function handleBlur (value: string, index: number) {
+  let entries = [...props.modelValue]
+  entries[index] = value
+  // When the user blurs the input (just tapping somewhere else or hitting
+  // a button), we need to remove any empty entries, unless it's the first
+  // item, which is the "wip" entry.
+  entries = entries.filter((entry, i) => {
+    return i === 0 || entry !== ''
+  })
   emit('update:modelValue', entries)
 }
 
@@ -19,7 +60,8 @@ function update (n: string, index: number) {
         v-if="index !== props.modelValue.length - 1 || n !== ''"
         type="text"
         :value="n"
-        @input="update($event.target.value, index)"
+        @input="handleInput($event.target.value, index)"
+        @blur="handleBlur($event.target.value, index)"
       >
     </template>
   </div>
